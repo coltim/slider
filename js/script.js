45bd94f9742873bfc06d9f97760bc7b40012fb3d
coltim <cseketomi1100@gmail.com>
2023-04-16 13:42:43 +0200

    page zoom

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
-var swiper = new Swiper('.mySwiper', {
+var mainSwiper = new Swiper('.mySwiper', {
   direction: 'vertical',
+  slidesPerView: 1,
+  zoom: {
+    maxRatio: 2,
+    minRatio: 1,
+  },
+  mousewheel: true,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination2',
+    dynamicBullets: true,
+    clickable: true,
+    type: 'fraction',
+  },
   pagination: {
     el: '.swiper-pagination',
-    clickable: true,
+    dynamicBullets: true,
   },
 });
+/*
+var fractionSwiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination2',
+    type: 'fraction',
+  },
+});
+*/
+/*
+
+var swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination2',
+    dynamicBullets: true,
+  },
+});
+
+mainSwiper.controller.control = swiper;*/
