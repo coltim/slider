2a07a95a8b9df03aed9c1fd7999fe133724c2ed5
coltim <cseketomi1100@gmail.com>
2023-04-18 23:17:49 +0200

    add language buttons

--- js/script.js
+++ js/script.js
@@ -1,30 +1,65 @@
 const cardsContainer = document.querySelector('#cardsContainer');
+const langButtonHU = document.getElementById('langButtonHU');
+const langButtonEN = document.getElementById('langButtonEN');
 console.log(cardsContainer);
-for (let i = 1; i <= 5; i++) {
-  cardsContainer.innerHTML += `<div class="swiper-slide">
-  <div class="swiper-zoom-container">
-    <img src="images/card${i}.jpg" alt="card" class="card" />
-  </div>
-</div>`;
+
+for (let i = 0; i <= 12; i++) {
+  fetch('images/menu0' + i + '.jpg', { method: 'HEAD' })
+    .then((response) => {
+      if (response.ok) {
+        console.log('File exists ' + i);
+        cardsContainer.innerHTML += `<div class="swiper-slide">
+        <div class="swiper-zoom-container">
+        <img src="images/menu0${i}.jpg" loading="lazy" class="card" alt="card" />
+        </div>
+      </div>`;
+      } else {
+        console.log('File does not exist ' + i);
+      }
+    })
+    .catch((error) => {
+      console.log('An error occurred: ', error);
+    });
 }
+
+langButtonEN.addEventListener('click', () => {
+  console.log('magyar gomb');
+  document.querySelectorAll('.swiper-slide').forEach((slide) => {
+    slide.style.backgroundColor = '#4d4d4d';
+  });
+});
+langButtonHU.addEventListener('click', () => {
+  console.log('magyar gomb');
+  document.querySelectorAll('.swiper-slide').forEach((slide) => {
+    slide.style.backgroundColor = '#faebd7';
+  });
+});
 
 var mainSwiper = new Swiper('.mySwiper', {
   direction: 'vertical',
-  slidesPerView: 1,
+
+  slidesPerView: 1.2,
+  lazy: true,
   zoom: {
     maxRatio: 2,
     minRatio: 1,
   },
   mousewheel: true,
-  spaceBetween: 30,
-  pagination: {
-    el: '.swiper-pagination2',
-    dynamicBullets: true,
-    clickable: true,
-    type: 'fraction',
-  },
+  spaceBetween: 0,
+  //centeredSlides: true,
   pagination: {
     el: '.swiper-pagination',
     dynamicBullets: true,
+    clickable: true,
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      direction: 'horizontal',
+    },
   },
 });
